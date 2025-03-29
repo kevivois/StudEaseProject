01b4eb71966cf374bf809813ba4e600839e2fe4c
AlexisKueny <alexis.kueny@outlook.com>
2025-03-29 11:54:42 +0100

    Structure and further developement on student dashboard page

--- frontend/src/app/pages/studentDashboard.js
+++ frontend/src/app/pages/studentDashboard.js
@@ -10,6 +10,7 @@ function StudentDashboard() {
   const [locationTerm, setLocationTerm] = useState('');
 
   return (
+    <>
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
         <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:space-x-4">
           <div className="flex-1 relative">
@@ -39,7 +40,12 @@ function StudentDashboard() {
             <FilterIcon />
           </IconButton>
         </div>
+        
+        <br></br>
+        <h2>Latest Offers</h2>
       </div>
+
+    </>
   );
 }
 
