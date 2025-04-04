4a4df9d4415d6be7053c1c0b0f3882d051938aef
kevin voisin <kevin.voisin187@gmail.com>
2025-04-04 18:34:29 +0200

    remove old js backend

--- studease-backend/pages/api/test/home.js
+++
@@ -1,6 +0,0 @@
-// pages/api/auth/login.js
-import { supabase } from '../../../lib/supabase';
-
-export default async function handler(req, res) {
-    res.status(200).json({ message: 'Hello' });
-}
