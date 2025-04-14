fd79820dd7a0865c6fc02141d80653e8592afeeb
kevin voisin <kevin.voisin187@gmail.com>
2025-04-14 10:56:03 +0200

    updated title + middleware cors

--- studease_backend/lib/middleware-helper.ts
+++ studease_backend/lib/middleware-helper.ts
@@ -105,7 +105,7 @@ export function getHeaders() {
   
     // Set CORS headers for all API routes
     headers.append('Access-Control-Allow-Credentials', "true")
-    headers.append('Access-Control-Allow-Origin', 'studease.jevs.ch') // frontend url
+    headers.append('Access-Control-Allow-Origin', 'https://studease.jevs.ch') // frontend url
     headers.append('Access-Control-Allow-Methods', 'GET,DELETE,PATCH,POST,PUT,OPTIONS')
     headers.append(
         'Access-Control-Allow-Headers',
